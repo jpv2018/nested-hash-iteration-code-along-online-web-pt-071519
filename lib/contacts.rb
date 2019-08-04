7aba919e68519b2881230217e21b633289df7d27
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-04 22:19:15 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == "Strawberry"
+          data.pop
+        end
+      end
+    end
+  end
 end
